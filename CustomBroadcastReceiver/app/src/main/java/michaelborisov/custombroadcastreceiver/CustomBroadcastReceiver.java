da544e0fcf407c263257725a5c6012de06d5c19a
michaelborisov <michaelborisovha@gmail.com>
2019-10-27 13:55:40 +0100

    Add second app

--- CustomBroadcastReceiver/app/src/main/java/michaelborisov/custombroadcastreceiver/CustomBroadcastReceiver.java
+++ CustomBroadcastReceiver/app/src/main/java/michaelborisov/custombroadcastreceiver/CustomBroadcastReceiver.java
@@ -4,10 +4,14 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.Toast;
 
 public class CustomBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d("Custom", intent.getAction());
+
+        Toast.makeText(
+                context, "SHOULD NOT BE SHOWN", Toast.LENGTH_LONG
+        ).show();
     }
 }
